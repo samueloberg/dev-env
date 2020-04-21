5d68d62f6fa576cd0a19133d6dbac4a6f90ec43e
Samuel Oberg <samueloberg@protonmail.com>
2020-04-20 20:38:36 -0700

    run YouCompleteMe setup commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,9 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mv ~/.vimrc ~/.vimrc.old
 cp .vimrc ~/.vimrc
 
+brew install cmake macvim python
+brew install mono go nodejs
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
+
 vim +PluginInstall +qall
